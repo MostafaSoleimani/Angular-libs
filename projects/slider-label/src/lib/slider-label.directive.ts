d769130366ca12d120e5a182fb84c2e287070ac9
MostafaSoleimani <88878976+MostafaSoleimani@users.noreply.github.com>
2022-06-28 16:56:39 +0430

    add direction support

--- projects/slider-label/src/lib/slider-label.directive.ts
+++ projects/slider-label/src/lib/slider-label.directive.ts
@@ -1,11 +1,25 @@
 import { animate, AnimationBuilder, style } from '@angular/animations';
 import { DOCUMENT } from '@angular/common';
-import { Directive, ElementRef, HostListener, AfterViewInit, Inject, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, AfterViewInit, Inject, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[ngxSliderLabel]'
 })
 export class SliderLabelDirective implements AfterViewInit {
+  /** direction 'rtl' | 'ltr' */
+  @Input() dir: 'rtl' | 'ltr' = 'ltr';
+  /** Animation Duration in ms */
+  @Input() dur: number = 1000;
+  /** Animation Duration for back to normal in ms */
+  @Input() comeBackTime: number = 40;
+  @Input() className: string = 'ngxSliderLabel';
+
+
+  get margin() {
+    return this.dir === 'rtl' ? 'marginRight' : 'marginLeft'
+  }
+
+
   span: any;
 
   get parentBound() {
@@ -31,6 +45,7 @@ export class SliderLabelDirective implements AfterViewInit {
     const text = this.elementRef.nativeElement.innerText;
     this.span = this.document.createElement('span');
     this.span.innerText = text;
+    this.span.classList.add(this.className);
     this.elementRef.nativeElement.innerText = '';
     this.elementRef.nativeElement.style.textOverflow = 'ellipsis';
     this.elementRef.nativeElement.style.overflow = 'hidden';
@@ -53,8 +68,8 @@ export class SliderLabelDirective implements AfterViewInit {
   makeAnimation(element: any) {
     // first define a reusable animation
     const myAnimation = this._builder.build([
-      style({ marginRight: 0 }),
-      animate(1000, style({ marginRight: -(this.childBound.width - this.parentBound.width + 10) + 'px' }))
+      style({ [this.margin]: 0 }),
+      animate(this.dur, style({ [this.margin]: -(this.childBound.width - this.parentBound.width + 10) + 'px' }))
     ]);
 
     // use the returned factory object to create a player
@@ -66,8 +81,8 @@ export class SliderLabelDirective implements AfterViewInit {
   removeAnimation(element: any) {
     // first define a reusable animation
     const myAnimation = this._builder.build([
-      // style({ marginRight: -(this.childBound.width - this.parentBound.width + 10) + 'px' }),
-      animate(40, style({ marginRight: '0px' }))
+      // style({ this.margin: -(this.childBound.width - this.parentBound.width + 10) + 'px' }),
+      animate(this.comeBackTime, style({ [this.margin]: '0px' }))
     ]);
 
     // use the returned factory object to create a player
